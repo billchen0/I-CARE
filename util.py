1b1a1e45d2f1e40154463a419dbe0709148bd8ae
Bill Chen <bc299@wil.egr.duke.edu>
2023-08-04 15:25:39 -0400

    completed functions for loading in patient clinical data, created exploratory plots

---
+++ util.py
@@ -0,0 +1,50 @@
+import re
+from pathlib import Path
+import pandas as pd
+
+
+### I/O Helper Functions ###
+
+# Function to find all patient id in training set
+def list_patient_id(root_path: Path):
+    patient_list = []
+    patient_dirs = [p for p in root_path.iterdir() if re.fullmatch(r"\d{4}", p.name)]
+    for patient in patient_dirs:
+        patient_list.append(patient.name)
+
+    return patient_list
+
+
+# Function to load patient clinical data
+def load_clinical_data(root_path: Path, patient_id: str):
+    load_path = root_path / patient_id
+    data_file = list(load_path.glob("*.txt"))[0]
+    with open(data_file, "r") as f:
+        data = f.read().split("\n")[:-1]
+
+    return data
+    
+
+
+### Compile Data Helper Functions ###
+
+def compile_clinical_data(root_path: Path):
+    patients = list_patient_id(root_path)
+    patient_data = []
+    for patient in patients:
+        # Add patient metadata as a list to the data list
+        patient_data.append(load_clinical_data(root_path, patient))
+
+    # Extract key and values from each metadata list
+    formatted_patient_data = []
+    for p_data in patient_data:
+        formatted_p_data = {item.split(":")[0].strip(): item.split(":")[1].strip() for item in p_data}
+        formatted_patient_data.append(formatted_p_data)
+    
+    # Create dataframe from formatted data and convert numeric columns to appropriate type
+    df = pd.DataFrame(formatted_patient_data)
+    df["Age"] = pd.to_numeric(df["Age"], errors="coerce")
+    df["TTM"] = pd.to_numeric(df["TTM"], errors="coerce")
+    df["ROSC"] = pd.to_numeric(df["ROSC"], errors="coerce")
+
+    return df
