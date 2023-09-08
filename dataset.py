37a0e201dac62cec22b42fbc3994baf0b2c36956
Bill Chen <bc299@bil.egr.duke.edu>
2023-09-08 22:32:37 +0000

    finished the data module set up for manual features

--- dataset.py
+++ dataset.py
@@ -1,7 +1,9 @@
 import torch 
 import pandas as pd
 import numpy as np
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader
+import lightning.pytorch as pl
+from sklearn.model_selection import train_test_split
 from util import transform_to_binary_labels
 
 
@@ -30,3 +32,41 @@ class ManualFeatureDataset(Dataset):
         label = torch.tensor(self.labels_df.loc[patient_id])
 
         return data, label
+    
+
+class ManualFeatureDataModule(pl.LightningDataModule):
+    def __init__(self, root_dir, labels_csv, batch_size=32):
+        super().__init__()
+        self.root_dir = root_dir
+        self.labels_csv = labels_csv
+        self.batch_size = batch_size
+
+    def setup(self, stage: str):
+        # List all patient ids
+        all_patient_ids = [dir_.name for dir_ in self.root_dir.iterdir()]
+        train_ids, temp_ids = train_test_split(all_patient_ids, test_size=0.3, random_state=42)
+        val_ids, test_ids = train_test_split(temp_ids, test_size=2/3, random_state=42)
+
+        # Create datasets for each split based on patient ids
+        self.train_dataset = ManualFeatureDataset(self.root_dir, 
+                                                  self.labels_csv, 
+                                                  patient_ids=train_ids)
+        self.val_dataset = ManualFeatureDataset(self.root_dir, 
+                                                self.labels_csv, 
+                                                patient_ids=val_ids)
+        self.test_dataset = ManualFeatureDataset(self.root_dir, 
+                                                 self.labels_csv, 
+                                                 patient_ids=test_ids)
+
+    def train_dataloader(self):
+        return DataLoader(self.train_dataset, 
+                          batch_size=self.batch_size, 
+                          shuffle=True)
+
+    def val_dataloader(self):
+        return DataLoader(self.val_dataset,
+                          batch_size=self.batch_size)
+
+    def test_dataloader(self):
+        return DataLoader(self.test_dataset,
+                          batch_size=self.batch_size)
