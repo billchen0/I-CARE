f801c30762f6c9bac281eaab8ccfe6220885603f
Bill Chen <bc299@wil.egr.duke.edu>
2023-09-20 20:19:13 -0400

    added another text file with all patients to include in this project. Made split data script to exclude patients and added some error handling

--- cnn-feature-extraction/split_data.py
+++ cnn-feature-extraction/split_data.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import ast
 import numpy as np
 
 
@@ -8,21 +9,31 @@ fs = 100
 seg_len = 10 #seconds
 data_per_seg = fs * seg_len
 
+# Get the list of patients to include in this program
+patients_path = Path("/home/bc299/icare/artifacts/include_patients.txt")
+with open(patients_path) as f:
+    include_patients = ast.literal_eval(f.readline())
+
 # Loop through the participants
 for patient in path_to_data.iterdir():
-    patient_id = patient.name
+    if patient.is_dir() and patient.name in include_patients:
+        patient_id = patient.name
 
-    for hour_file in patient.iterdir():
-        hour_id = hour_file.stem.split("_")[2]
-        hour_data = np.load(hour_file)
+        for hour_file in patient.iterdir():
+            hour_id = hour_file.stem.split("_")[2]
+            hour_data = np.load(hour_file)
 
-        num_segments = len(hour_data) // data_per_seg
-        segments = np.array_split(hour_data, num_segments)
+            num_segments = len(hour_data) // data_per_seg
+            try:
+                segments = np.array_split(hour_data, num_segments)
+            except ValueError:
+                print(f"Encountered number section=0 error, patient id: {patient_id}")
+                continue
 
-        save_folder = save_path / patient_id
-        save_folder.mkdir(parents=True, exist_ok=True)
+            save_folder = save_path / patient_id
+            save_folder.mkdir(parents=True, exist_ok=True)
 
-        for idx, segment in enumerate(segments):
-            filename = f"{patient_id}_{hour_id}_{idx:03}_EEG.npy"
-            filepath = save_folder / filename
-            np.save(filepath, segment)
+            for idx, segment in enumerate(segments):
+                filename = f"{patient_id}_{hour_id}_{idx:03}_EEG.npy"
+                filepath = save_folder / filename
+                np.save(filepath, segment)
