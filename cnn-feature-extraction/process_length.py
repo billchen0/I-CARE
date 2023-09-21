c24f80023d242bbd8d7e938a4b28a5a408cbd25d
Bill Chen <bc299@wil.egr.duke.edu>
2023-09-21 19:14:05 -0400

    finished and ran the script to process the 10s segment lengths

--- cnn-feature-extraction/process_length.py
+++ cnn-feature-extraction/process_length.py
@@ -2,22 +2,30 @@ import numpy as np
 from pathlib import Path
 from multiprocessing import Pool, cpu_count
 
-
 def check_eeg_length(eeg_file):
     eeg = np.load(eeg_file)
-    if eeg.shape[0] < 1000:
-        return eeg_file.name
+    if eeg.shape[0] > 1000:
+        eeg = eeg[:1000]  # Truncate segments larger than 1000
+        np.save(eeg_file, eeg)
+    elif 800 <= eeg.shape[0] < 1000:
+        # Impute missing values with previous values
+        diff = 1000 - eeg.shape[0]
+        eeg = np.concatenate((eeg, eeg[-diff:]))
+        np.save(eeg_file, eeg)
+    elif eeg.shape[0] < 800:
+        eeg_file.unlink()  # Remove the file
+        return eeg_file.parent.name  # Return patient name for discarded segments
     return None
-
 
 if __name__ == "__main__":
     print(f"Number of CPUs: {cpu_count()}")
     path_to_data = Path("/media/hdd1/i-care/10s")
-    all_eeg_files = [eeg_file for patient in path_to_data.iterdir() for eeg_file in patient.iterdir()]
+    all_patients = list(path_to_data.iterdir())
 
-    with Pool(cpu_count()) as p:
-        results = p.map(check_eeg_length, all_eeg_files)
-    
-    short_seq = [res for res in results if res is not None]
-
-    print(f"Number of sequences that are short: {len(short_seq)}")
+    for patient in all_patients:
+        patient_files = list(patient.iterdir())
+        with Pool(cpu_count()) as p:
+            discarded_results = p.map(check_eeg_length, patient_files)
+        
+        discarded_count = len([res for res in discarded_results if res is not None])
+        print(f"Processing completed for patient {patient.name}. {discarded_count} segments discarded.")
