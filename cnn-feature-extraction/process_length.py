eb1ddeb2cb7d4505d2cf2d1221c7e1cafc400180
Bill Chen <bc299@wil.egr.duke.edu>
2023-09-22 11:52:23 -0400

    added to process length script to only process patients that haven't been already processed

--- cnn-feature-extraction/process_length.py
+++ cnn-feature-extraction/process_length.py
@@ -1,6 +1,9 @@
 import numpy as np
 from pathlib import Path
 from multiprocessing import Pool, cpu_count
+import sys
+sys.path.append("..")
+from util import load_split_ids
 
 def check_eeg_length(eeg_file):
     eeg = np.load(eeg_file)
@@ -19,11 +22,17 @@ def check_eeg_length(eeg_file):
 
 if __name__ == "__main__":
     print(f"Number of CPUs: {cpu_count()}")
-    path_to_data = Path("/media/hdd1/i-care/10s")
-    all_patients = list(path_to_data.iterdir())
+    processed_patients_path = Path("/media/hdd1/i-care/10s")
+    split_path = Path("/home/bc299/icare/artifacts")
+    processed_patients = [p.name for p in processed_patients_path.iterdir()]
+    train_ids, val_ids, test_ids = load_split_ids(split_path)
+    all_patients = train_ids + val_ids + test_ids
+    
+    process_patients = [p for p in all_patients if p not in processed_patients]
 
-    for patient in all_patients:
-        patient_files = list(patient.iterdir())
+    for patient in process_patients:
+        patient_path = Path("/media/hdd1/i-care/processed") / patient
+        patient_files = list(patient_path.iterdir())
         with Pool(cpu_count()) as p:
             discarded_results = p.map(check_eeg_length, patient_files)
         
